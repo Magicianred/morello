582602062bc7e49e81564f35ecbfe96c62599a47
jbuyu <jbuyu@ymail.com>
2021-01-11 16:03:29 +0300

    misc fixes

--- src/components/App.js
+++ src/components/App.js
@@ -3,26 +3,30 @@ import TrelloList from "./TrelloList";
 import { connect } from "react-redux";
 import TrelloActionButton from "./TrelloActionButton";
 
+import { DragDropContext } from "react-beautiful-dnd";
+
 class App extends Component {
   render() {
     const { lists } = this.props;
     return (
-      <div style={styles.appContainer}>
-        <h3 style={styles.header}>Morello</h3>
-        <div style={styles.listContainer}>
-          {lists.map((list) => {
-            return (
-              <TrelloList
-                listID={list.id}
-                key={list.id}
-                title={list.title}
-                cards={list.cards}
-              />
-            );
-          })}
-          <TrelloActionButton list />
+      <DragDropContext>
+        <div style={styles.appContainer}>
+          <h3 style={styles.header}>Morello</h3>
+          <div style={styles.listContainer}>
+            {lists.map((list) => {
+              return (
+                <TrelloList
+                  listID={list.id}
+                  key={list.id}
+                  title={list.title}
+                  cards={list.cards}
+                />
+              );
+            })}
+            <TrelloActionButton list />
+          </div>
         </div>
-      </div>
+      </DragDropContext>
     );
   }
 }
